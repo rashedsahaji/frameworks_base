fb814326718c1986e3e1ff90541c0b72ccb25528
xyyx <xyyx@mail.ru>
2018-01-30 14:35:41 +0800

    Add toggle for face auto unlock (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8591,6 +8591,13 @@ public final class Settings {
          */
         public static final String NAVIGATION_BAR_VISIBLE = "navigation_bar_visible";
 
+	/**
+         * Enable face auto unlock on secure lock screens
+         *
+         * @hide
+         */
+        public static final String FACE_AUTO_UNLOCK = "face_auto_unlock";
+
         /**
          * This are the settings to be backed up.
          *
