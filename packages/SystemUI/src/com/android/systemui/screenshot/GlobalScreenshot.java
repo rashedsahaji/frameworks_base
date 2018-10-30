955850e5afeee578a43108337b8b6d920d7295c6
darkbeast69 <guptaaryan189@gmail.com>
2018-10-30 12:33:04 +0000

    its lean boi

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -78,7 +78,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.internal.messages.nano.SystemMessageProto.SystemMessage;
-import com.android.internal.util.aospextended.AEXUtils;
+import com.android.internal.util.lean.LeanUtils;
 import com.android.systemui.R;
 import com.android.systemui.SystemUI;
 import com.android.systemui.util.NotificationChannels;
@@ -145,7 +145,7 @@ class SaveImageInBackgroundTask extends AsyncTask<Void, Void, Void> {
         String imageDate = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date(mImageTime));
         mImageFileName = String.format(SCREENSHOT_FILE_NAME_TEMPLATE, imageDate);
         final PackageManager pm = context.getPackageManager();
-        ActivityInfo info = AEXUtils.getRunningActivityInfo(context);
+        ActivityInfo info = LeanUtils.getRunningActivityInfo(context);
         if (info != null) {
             CharSequence appName = pm.getApplicationLabel(info.applicationInfo);
             boolean onKeyguard = context.getSystemService(KeyguardManager.class).isKeyguardLocked();
