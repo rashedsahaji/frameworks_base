d27a49dc80ba6caf2d896af0b80065f7f745d667
ezio84 <brabus84@gmail.com>
2018-10-31 18:38:16 +0100

    Allow to set a full gesture navbar (swipe left for back action) [1/2]
    swipe left for back button action
    keeps showing the back button when pinning or when showing pin unlock
    
    This doesn't break left swipe action to scroll apps when Overview is showing
    after swiping right on navbar
    
    Change-Id: I8b6a14d444875cb9be9ff41b26852a806583408f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4642,6 +4642,17 @@ public final class Settings {
         public static final String ANSWER_VOLUME_BUTTON_BEHAVIOR_ANSWER = "call_volume_answer";
 
         /**
+         * @hide
+         */
+        public static final String FULL_GESTURE_NAVBAR = "full_gesture_navbar";
+
+        /**
+         * Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
+       /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
